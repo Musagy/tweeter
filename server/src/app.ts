3eb0afd7b3b68bb1f298d6fe737e5ba50e9c825d
Musagy <diego.1203.dm@gmail.com>
2023-01-13 13:55:38 -0500

    update cors 🦺

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,7 @@ const app = express()
 // server config
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://tweeter-musagy.vercel.app/login"],
   })
 )
 app.use(express.json())
